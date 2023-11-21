a057dc19d943e0038d4ee8bc54096d9ec3ade987
fishinitself <mikhail.ershov@gmail.com>
2023-11-21 15:46:36 +0200

    @JsonProperty in WLProperties

--- validation/src/main/kotlin/org/octopusden/octopus/components/automation/task/WLProperties.kt
+++ validation/src/main/kotlin/org/octopusden/octopus/components/automation/task/WLProperties.kt
@@ -1,11 +1,14 @@
 package org.octopusden.octopus.components.automation.task
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.octopusden.octopus.tools.wl.validation.validator.CopyrightValidator
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class WLProperties(
+    @JsonProperty("contains")
     wContains: List<String>,
+    @JsonProperty("patterns")
     wPatterns: List<Regex>,
     val exceptions: List<String>,
     val restricted: String
